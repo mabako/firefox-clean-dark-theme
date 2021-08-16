a4c3f004d04cc773b6e94b09bd8bb524439206c2
Marcus Bauer <marcus@bauer.wf>
2021-08-16 10:04:58 +0200

    ~

---
+++ user.js
@@ -0,0 +1,8 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.proton.enabled", true);
+
+user_pref("browser.compactmode.show", true);
+
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.backdrop-filter.enabled", true);
+
